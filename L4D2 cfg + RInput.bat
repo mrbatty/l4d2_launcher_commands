101b41d028b6d4aec5f678a708028f3ad6981305
mrbatty <69885581+mrbatty@users.noreply.github.com>
2020-08-19 04:06:48 -0500

    Update L4D2 cfg + RInput.bat

--- L4D2 cfg + RInput.bat
+++ L4D2 cfg + RInput.bat
@@ -1,4 +1,4 @@
-start "steam.exe" "C:\Steam\steam.exe" -applaunch 550 -noforcemaccel -noforcemspd -useforcedmparms -novid -nojoy -nohltv -nosteamcontroller -noff -nomsaa -nominidumps -reuse -high -forcenovsync -forcestartupmenu -force_vendor_id 0x10de -force_device_id 0x1183 -floathdr -snoforceformat -softparticlesdefaulton -no_compressed_verts -wavonly -threads 4 -tickrate 128 -noaafonts -console -fov +fov
+start "steam.exe" "C:\Steam\steam.exe" -applaunch 550 -noforcemaccel -noforcemspd -useforcedmparms -novid -nojoy -nohltv -nosteamcontroller -noff -nomsaa -nominidumps -reuse -high -forcenovsync -forcestartupmenu -floathdr -snoforceformat -softparticlesdefaulton -no_compressed_verts -wavonly -threads 4 -noaafonts -console -fov +fov
 TIMEOUT /t 10 /nobreak
 
 START C:\RInput\RInput left4dead2.exe
