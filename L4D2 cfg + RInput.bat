f5db97e6ff4d38953062cd1b5043e0670a47b2f3
mrbatty <69885581+mrbatty@users.noreply.github.com>
2020-08-18 22:59:32 -0500

    Add files via upload

---
+++ L4D2 cfg + RInput.bat
@@ -0,0 +1,10 @@
+start "steam.exe" "C:\Steam\steam.exe" -applaunch 550 -noforcemaccel -noforcemspd -useforcedmparms -novid -nojoy -nohltv -nosteamcontroller -noff -nomsaa -nominidumps -reuse -high -forcenovsync -forcestartupmenu -force_vendor_id 0x10de -force_device_id 0x1183 -floathdr -snoforceformat -softparticlesdefaulton -no_compressed_verts -wavonly -threads 4 -tickrate 128 -noaafonts -console -fov +fov
+TIMEOUT /t 10 /nobreak
+
+START C:\RInput\RInput left4dead2.exe
+
+TIMEOUT /t 1 /nobreak
+
+EXIT
+
+-/-
